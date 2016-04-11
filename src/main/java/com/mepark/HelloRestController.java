3f6667cf2a0e09c2c33909b2a30c362e5ba1a761
hipark <hipark@github.com>
2016-04-11 22:45:10 +0900

    Todolist Start Project

---
+++ src/main/java/com/mepark/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.mepark;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+	@RequestMapping("/hello")
+	public String Hello() {
+		return "Hello World~!";
+	}
+}
---
+++ src/main/java/com/mepark/TodolistStartApplication.java
@@ -0,0 +1,12 @@
+package com.mepark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistStartApplication.class, args);
+	}
+}
